b2b2127efa1a84c554f3cb588573802f1ad41118
STOBOAT <stoboat2015@gmail.com>
2023-06-24 22:35:48 +0000

    Created a basic Whats_up_world programme

---
+++ README.md
@@ -0,0 +1,1 @@
+This Repo lets me try my hands on knlowledge acquired
---
+++ Whats_up.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *Description: Prints "Whats up world"
+  *Return: 0
+  */
+
+int main(void)
+{
+	printf("Whats up world\n");
+	return (0);
+}
